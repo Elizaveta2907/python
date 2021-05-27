5e84a6448bd623d2cf35400dbf831f2ec1078ad3
Elizaveta2907 <84903922+Elizaveta2907@users.noreply.github.com>
2021-05-27 12:38:52 +0300

    Delete Работа с файлами.py

--- Работа с файлами.py
+++
@@ -1,8 +0,0 @@
-# Вывести на экран первые пять строк содержимого (включая заголовки) с помощью цикла for
-
-with open('tour_de_france.csv') as f:
-    counter = 0
-    for line in f:
-        if counter < 5:
-            print(line)
-            counter += 1
